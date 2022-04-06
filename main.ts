c5dc0e04fb29b12855f61bf8667c1408a0934f32
natgo <xenhud@protonmail.com>
2022-04-07 01:06:33 +0300

    Initial commit

---
+++ main.ts
@@ -0,0 +1,87 @@
+import { readFileSync } from "fs";
+import { ocrSpace } from "ocr-space-api-wrapper";
+
+async function main() {
+  var file = readFileSync("./wk2022-03-30.csv", "utf-8"),
+    contents: string;
+  try {
+    contents = file;
+  } catch (err) {
+    console.log("There has been an error parsing your JSON.");
+    console.log(err);
+  }
+  //var csv is the CSV file with headers
+  function csvJSON(csv: string) {
+    var lines = csv.split("\n");
+
+    var result = [];
+
+    var headers = lines[0].split(",");
+
+    for (var i = 1; i < lines.length; i++) {
+      var obj = {};
+      var currentline = lines[i].split(",");
+
+      for (var j = 0; j < headers.length; j++) {
+        obj[headers[j]] = currentline[j];
+      }
+
+      result.push(obj);
+    }
+
+    //return result; //JavaScript object
+    return result; //JSON
+  }
+  let arr = csvJSON(contents);
+  let res = arr.filter(air);
+  function air(vehicle: { cls: string; }) {
+    return vehicle.cls === "Aviation";
+  }
+  let sel = res.filter(filterhighbr);
+  function filterhighbr(vehicle: { rb_br: number; }) {
+    return vehicle.rb_br <= 3.3;
+  }
+  function filterlowbr(vehicle: { rb_br: number; }) {
+    return vehicle.rb_br >= 2.3;
+  }
+  let set = sel.filter(filterlowbr);
+  try {
+    console.log("querying");
+    const res1 = await ocrSpace("ss.png", { OCREngine: 2 });
+    console.log(res1);
+    let aray = res1.ParsedResults[0].ParsedText.split("\n");
+    console.log(aray);
+    let result = [];
+    aray.forEach((element: string) => {
+      console.log(element.replace(/\s/g,"_"));
+      console.log(element);
+      if (element == "Spitfire_Mk_la") {
+        console.log("spitit");
+        element = "Spitfire_Mk_Ia";
+      }
+      if (element[element.length - 1] == ".") {
+        console.log(element);
+      } else {
+        for (let index = 0; index < set.length; index++) {
+          const elemen = set[index];
+          if (elemen.name == element) {
+            let object = {
+              name: elemen.name,
+              br: elemen.rb_br,
+            };
+            result.push(object);
+          }
+        }
+      }
+    });
+    result.sort(function (a, b) {
+      let y = a.br;
+      let x = b.br;
+      return x - y;
+    });
+    console.log(result);
+  } catch (error) {
+    console.error(error);
+  }
+}
+main();
